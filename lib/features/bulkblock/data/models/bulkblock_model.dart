d36440da07e3ca671377d24510985fb2d9ce7b43
Ajay Titus <ajaytitus1386@gmail.com>
2022-02-05 14:39:54 +0530

    feat: add bulk block model

---
+++ lib/features/bulkblock/data/models/bulkblock_model.dart
@@ -0,0 +1,68 @@
+import 'dart:convert';
+
+import 'package:gis_task/features/bulkblock/domain/entities/bulkblock.dart';
+
+List<BulkBlockModel> bulkblocksFromJson(String str) {
+  final responseJsonMap = json.decode(str);
+  final dataMap = responseJsonMap["Data"];
+
+  return List<BulkBlockModel>.from(
+      dataMap.map((bulkBlockJson) => BulkBlockModel.fromMap(bulkBlockJson)));
+}
+
+String bulkblockToJson(List<BulkBlockModel> bulkblocks) {
+  final result =
+      List<dynamic>.from(bulkblocks.map((bulkblock) => bulkblock.toMap()));
+  final jsonMap = {
+    "Data": result,
+  };
+  return json.encode(jsonMap);
+}
+
+class BulkBlockModel extends BulkBlock {
+  const BulkBlockModel({
+    required String finCode,
+    required String clientName,
+    required String dealType,
+    required int quantity,
+    required double value,
+    required double tradePrice,
+    required DateTime dealDate,
+    required String exchange,
+  }) : super(
+          finCode: finCode,
+          clientName: clientName,
+          dealType: dealType,
+          quantity: quantity,
+          value: value,
+          tradePrice: tradePrice,
+          dealDate: dealDate,
+          exchange: exchange,
+        );
+
+  factory BulkBlockModel.fromMap(Map<String, dynamic> map) {
+    return BulkBlockModel(
+      finCode: map["FinCode"] ?? '',
+      clientName: map["ClientName"] ?? '',
+      dealType: map["DealType"] ?? '',
+      quantity: int.parse(map["Quantitiy"]),
+      value: double.parse(map["Value"]),
+      tradePrice: double.parse(map["TradePrice"]),
+      dealDate: DateTime.parse(map["DateTime"]),
+      exchange: map["Exchange"] ?? '',
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'FinCode': finCode,
+      'ClientName': clientName,
+      'DealtType': dealType,
+      'Quantity': quantity.toString(),
+      'Value': value.toString(),
+      'TradePrice': tradePrice.toString(),
+      'Dealdate': dealDate.toString(),
+      'Exchange': exchange
+    };
+  }
+}
