5704b3765ea0236aa7027a2da5d88bb6c3ad128e
Ajay Titus <ajaytitus1386@gmail.com>
2022-02-05 09:55:45 +0530

    feat: add bulkblock entity

---
+++ lib/features/bulkblock/domain/entities/bulkblock.dart
@@ -0,0 +1,35 @@
+import 'package:equatable/equatable.dart';
+
+class BulkBlock extends Equatable {
+  final String finCode;
+  final String clientName;
+  final String dealType;
+  final int quantity;
+  final int value;
+  final int tradePrice;
+  final DateTime dealDate;
+  final String exchange;
+
+  const BulkBlock({
+    required this.finCode,
+    required this.clientName,
+    required this.dealType,
+    required this.quantity,
+    required this.value,
+    required this.tradePrice,
+    required this.dealDate,
+    required this.exchange,
+  });
+
+  @override
+  List<Object?> get props => [
+        finCode,
+        clientName,
+        dealType,
+        quantity,
+        value,
+        tradePrice,
+        dealDate,
+        exchange
+      ];
+}
