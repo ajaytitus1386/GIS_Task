40df114aeeae63922646def94693ba32c8bf3b9a
Ajay Titus <ajaytitus1386@gmail.com>
2022-02-06 13:24:27 +0530

    style: add colors, size and text styles

---
+++ lib/features/bulkblock/presentation/styling/text_styles.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:gis_task/features/bulkblock/presentation/styling/color_palettes.dart';
+
+TextStyle buildTextButtonStyle() {
+  return TextStyle(color: blueNCS, fontSize: 20, fontWeight: FontWeight.w400);
+}
+
+TextStyle buildDealTypeStyle({required bool isSelected}) {
+  return TextStyle(
+      color: isSelected ? textGray : highlight,
+      fontSize: 20,
+      fontWeight: FontWeight.w500);
+}
