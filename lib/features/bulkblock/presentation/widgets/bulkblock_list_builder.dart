f9eddfa579c9315de70f0d5bbeb48228c6d53fff
Ajay Titus <ajaytitus1386@gmail.com>
2022-02-07 20:47:06 +0530

    feat: add filter by client name

--- lib/features/bulkblock/presentation/widgets/bulkblock_list_builder.dart
+++ lib/features/bulkblock/presentation/widgets/bulkblock_list_builder.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:gis_task/features/bulkblock/domain/entities/bulkblock.dart';
+import 'package:gis_task/features/bulkblock/presentation/styling/text_styles.dart';
 import 'package:gis_task/features/bulkblock/presentation/widgets/bulk_deal_card.dart';
 
 class BulkBlockListBuilder extends StatefulWidget {
   final List<BulkBlock> bulkBlockList;
-  const BulkBlockListBuilder({Key? key, required this.bulkBlockList})
+  final String clientNameToFilter;
+  const BulkBlockListBuilder(
+      {Key? key, required this.bulkBlockList, required this.clientNameToFilter})
       : super(key: key);
 
   @override
@@ -14,18 +17,34 @@ class BulkBlockListBuilder extends StatefulWidget {
 class _BulkBlockListBuilderState extends State<BulkBlockListBuilder> {
   @override
   Widget build(BuildContext context) {
+    //Filters the bulkBlock list based on the search bar input
+    List<BulkBlock> filteredBulkBlockList = widget.bulkBlockList
+        .where((bulkBlock) => bulkBlock.clientName
+            .contains(widget.clientNameToFilter.toUpperCase()))
+        .toList();
+    if (widget.clientNameToFilter == '') {
+      filteredBulkBlockList = widget.bulkBlockList;
+    }
     return Expanded(
-      child: Scrollbar(
-        isAlwaysShown: true,
-        child: ListView.builder(
-          shrinkWrap: true,
-          scrollDirection: Axis.vertical,
-          itemCount: widget.bulkBlockList.length,
-          itemBuilder: (context, index) {
-            return BulkDealCard(bulkBlock: widget.bulkBlockList[index]);
-          },
-        ),
-      ),
+      child: filteredBulkBlockList.isNotEmpty
+          ? Scrollbar(
+              isAlwaysShown: true,
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.vertical,
+                itemCount: filteredBulkBlockList.length,
+                itemBuilder: (context, index) {
+                  return BulkDealCard(bulkBlock: filteredBulkBlockList[index]);
+                },
+              ),
+            )
+          : Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              child: Text(
+                'No results found',
+                style: buildNoResultsStyle(),
+              ),
+            ),
     );
   }
 }
