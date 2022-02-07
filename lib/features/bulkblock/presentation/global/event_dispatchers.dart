c8291f62941a2ff9a1ac164a616b018c1264fa74
Ajay Titus <ajaytitus1386@gmail.com>
2022-02-07 20:44:52 +0530

    refactor: dispatch methods for bulk and block

--- lib/features/bulkblock/presentation/global/event_dispatchers.dart
+++ lib/features/bulkblock/presentation/global/event_dispatchers.dart
@@ -21,3 +21,23 @@ void dispatchGetBlockDealsByDealType(
   BlocProvider.of<BulkblockBloc>(context)
       .add(GetBlockDealsByDealType(dealTypeToSelect));
 }
+
+void getAllBlockDealsByDealType(BuildContext context, String dealTypeSelected) {
+  if (dealTypeSelected == 'All') {
+    dispatchGetAllBlockDeals(context);
+  } else if (dealTypeSelected == 'Buy') {
+    dispatchGetBlockDealsByDealType(context, 'Buy');
+  } else if (dealTypeSelected == 'Sell') {
+    dispatchGetBlockDealsByDealType(context, 'Sell');
+  }
+}
+
+void getAllBulkDealsByDealType(BuildContext context, String dealTypeSelected) {
+  if (dealTypeSelected == 'All') {
+    dispatchGetAllBulkDeals(context);
+  } else if (dealTypeSelected == 'Buy') {
+    dispatchGetBulkDealsByDealType(context, 'Buy');
+  } else if (dealTypeSelected == 'Sell') {
+    dispatchGetBulkDealsByDealType(context, 'Sell');
+  }
+}
