f208245fe00d345430fc1f7c0bdd01bd6f47d962
Ajay Titus <ajaytitus1386@gmail.com>
2022-02-08 17:18:59 +0530

    docs: add doc comments to repo

--- lib/features/bulkblock/domain/repos/bulkblock_repo.dart
+++ lib/features/bulkblock/domain/repos/bulkblock_repo.dart
@@ -3,6 +3,13 @@ import 'package:gis_task/core/error/failures.dart';
 import 'package:gis_task/features/bulkblock/domain/entities/bulkblock.dart';
 
 abstract class BulkBlockRepo {
+  /// Fetch Bulk Deals from API endpoint
+  ///
+  /// Returns [ServerFailure] if data could not be fetched
   Future<Either<Failure, List<BulkBlock>>> fetchBulkDealsFromApi();
+
+  /// Fetches Block deals from API endpoint
+  ///
+  /// Returns [ServerFailure] if data could not be fetched
   Future<Either<Failure, List<BulkBlock>>> fetchBlockDealsFromApi();
 }
