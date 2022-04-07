1688214fd70b2460e2af9280b941c6229d154931
Ajay Titus <ajaytitus1386@gmail.com>
2022-04-07 13:31:49 +0530

    refactor: change provider to Cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:gis_task/features/bulkblock/presentation/bloc/bulkblock_bloc.dart';
+import 'package:gis_task/core/network/network_info.dart';
+import 'package:gis_task/features/bulkblock/data/datasources/bulkblock_remote_data_sources.dart';
+import 'package:gis_task/features/bulkblock/data/repos/bulkblock_repo_impl.dart';
+import 'package:gis_task/features/bulkblock/presentation/cubit/bulkcube_cubit.dart';
 import 'package:gis_task/features/bulkblock/presentation/pages/home_page.dart';
 import 'package:gis_task/features/bulkblock/presentation/widgets/splash_screen_widget.dart';
-import 'package:gis_task/injection_container.dart';
-import 'injection_container.dart' as di;
+import 'package:http/http.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await di.init();
-  SystemChrome.setPreferredOrientations(
-      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) {
-    runApp(const MyApp());
-  });
+  runApp(const RootApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class RootApp extends StatelessWidget {
+  const RootApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<BulkblockBloc>(
-      create: (_) => sl<BulkblockBloc>(),
+    return BlocProvider<BulkcubeCubit>(
+      create: (_) => BulkcubeCubit(
+        BulkBlockRepoImpl(
+          remoteDataSource: BulkBlockRemoteDataSourceImpl(client: Client()),
+          networkInfo: NetworkInfoImpl(
+            InternetConnectionChecker(),
+          ),
+        ),
+      ),
       child: MaterialApp(
           title: 'GIS Task',
           theme: ThemeData(
